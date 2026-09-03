0010d7487b4e14103671d9a229c957d9d0865e7d
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-02 21:47:11 -0400

    Initial build scripts for supported targets
    Change-Id: Ie26f9912ec2c4b9f972b7a81fc8406dd152026e5

---
+++ build_odroidc4.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR"
+
+export SOURCE_DATE_EPOCH=$(git -C "../u-boot" log -1 --pretty=%ct HEAD)
+
+BOARD=sm1_odroidc4_v1
+FIP_DIR=fip-collect-g12a-odroidc4-odroidg12-v2015.01-20210623-153349
+UBOOT_BIN="${SCRIPT_DIR}/uboot-bins/u-boot.bin"
+
+LINEAGE_ROOT="$(cd "$SCRIPT_DIR/../../.." && pwd)"
+DEVICE_PATH="${LINEAGE_ROOT}/device/hardkernel/odroidc4"
+VENDOR_PATH="${LINEAGE_ROOT}/vendor/hardkernel/odroidc4"
+
+[[ -d "${VENDOR_PATH}" ]] && mkdir -p "${VENDOR_PATH}/radio"
+
+./build.sh "$BOARD"
+./generate-bins-new.sh "$FIP_DIR" ../u-boot/build/u-boot.bin "${BOARD}-base"
+[[ -d "${VENDOR_PATH}" ]] && cp "$UBOOT_BIN" "${VENDOR_PATH}/radio/bootloader.img"
+
+if [[ -d "${DEVICE_PATH}" ]]; then
+    cd "$DEVICE_PATH" && ./setup-makefiles.py
+fi
+if [[ -d "${VENDOR_PATH}" ]]; then
+    cd "$VENDOR_PATH" && git add -A && git commit -m "odroidc4: Update bootloader image prebuilt"
+fi
